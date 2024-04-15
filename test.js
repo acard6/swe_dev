25345f4e50742da9f67a21f937cccf991a859d50
alan <acard6@illinois.edu>
2024-04-15 15:23:47 -0500

    learning css, js, html

---
+++ test.js
@@ -0,0 +1,23 @@
+// async function getProfile(accessToken) {
+//     let accessToken = localStorage.getItem('access_token');
+  
+//     const response = await fetch('https://api.spotify.com/v1/me', {
+//       headers: {
+//         Authorization: 'Bearer ' + accessToken
+//       }
+//     });
+  
+//     const data = await response.json();
+//   }
+
+
+// const APIController = (function myAPI(){
+//     const clientID = "4d95d55f4baa48bfa1b250b737e14b74";
+
+//     console.log(clientID);
+// });
+
+let x = 5;
+x += 4;
+
+console.log(x);
